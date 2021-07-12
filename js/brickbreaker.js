2a753dd237db591a5057af6775452a4a57e042a3
EvaRoets <eva_roets@hotmail.com>
2021-07-12 10:46:11 +0200

    Add basis structure HTML and CSS

---
+++ js/brickbreaker.js
@@ -0,0 +1,19 @@
+//Phase 1
+//TODO create html canvas
+//TODO create bricks
+//TODO create ball
+//TODO create paddle
+//TODO add more todos 
+
+//Phase 2
+//TODO make bricks disappear when hit by the ball
+//TODO make ball move
+//TODO enable keyboard controls paddle
+//TODO enable mouse controls paddle
+//TODO
+
+//Phase 3
+//TODO create scoreboard
+//TODO keep score when ball hits bricks
+
+
