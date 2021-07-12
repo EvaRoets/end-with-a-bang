7c530b7be7696bb4c3bffbd160cfb02d7775e378
EvaRoets <eva_roets@hotmail.com>
2021-07-12 12:32:34 +0200

    Make ball bounce off walls

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -5,39 +5,53 @@ let context = canvas.getContext("2d");
 
 //TODO create moving ball  / define a drawing loop
 //define starting point in canvas in variables x and y
-//CHECK are these the correct starting points?
 let x = canvas.width/2;
-let y = canvas.height-30;
+let y = canvas.height -30;
 
-//define the position the circle is drawn at
-//CHECK why 2 and -2?
-let xDrawn = 2;
-let yDrawn = -2;
+//define the position + directs the circle is drawn
+let xDrawn = 5; // negative = left, positive = right // also changes speed!
+let yDrawn = -5; // negative = up, positive = down
 
-function draw() {
-    // clear frame after every interval to make ball instead of line
-    context.clearRect(0, 0, canvas.width, canvas.height);
-
-    // make ball move after every interval
+// make ball
+let ballRadius = 5;
+function drawBall() {
     context.beginPath();
-    context.arc(x, y, 3, 0, Math.PI*2);
-    context.fillStyle = "#0095DD";
+    context.arc(x, y, ballRadius, 0, Math.PI*2);
+    context.fillStyle = "#ff1493";
     context.fill();
     context.closePath();
+
+    //TODO make ball bounce off the walls/ define start + end canvas
+    // x = min 5 max 295
+    // y = min 5 max 145
+
+    //top + bottom wall
+    if(y + yDrawn <= 1 //if vertical starting point on y axis < 0 (it goes outside top canvas wall)
+        || y + yDrawn > canvas.height-1) {  //if vertical starting point on y axis > canvas height (it goes outside bottom canvas wall)
+        yDrawn = -yDrawn; //reverse direction on y axis = bounce
+    }
+
+    //left + right wall
+    if(x + xDrawn <= 1 //if horizontal starting point on x axis < 0 (it goes outside left canvas wall)
+        || x + xDrawn > canvas.width-1) {  //if horizontal starting point on x axis > canvas width (it goes outside right canvas wall)
+        xDrawn = -xDrawn; //reverse direction on x axis = bounce
+    }
+}
+
+function moveBall() {
+    // clear frame after every interval to make ball instead of line
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
     //update x and y to make ball appear in new position on every update
     x += xDrawn;
     y += yDrawn;
 }
-setInterval(draw, 40); // change this timeout to make the ball got faster/slower
+
+setInterval(moveBall, 80); // change this timeout to make the ball got faster/slower, lower number = faster
 
 
 
-// x = min 5 max 295
-// y = min 5 max 145
 
-//TODO ensure all elements stay in canvas
-
-//TODO make ball bounce off the walls
 
 //Phase 2
 //TODO create paddle
