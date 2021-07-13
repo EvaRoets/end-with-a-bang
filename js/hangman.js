4a076303149a20b1176e23b56f6a112d761fdcfa
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-13 10:56:34 +0200

    Add stages images - hangman

--- js/hangman.js
+++ js/hangman.js
@@ -1,6 +1,6 @@
 const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const words = ["refrigerator", "swimmingpool", "wardrobe", "carpet", "television"];
-let lives = 1;
+const words = ["refrigerator", "swimmingpool", "wardrobe", "carpet", "television", "cat", "dog", "home", "glass", "mouse"];
+let lives = 7;
 
 
 // Create buttons per letter
@@ -16,51 +16,67 @@ let createButtons = () => {
 }
 createButtons()
 
-// Generate a random word from the list & create letter array of this word
-
-let selectedWord = words[Math.floor(Math.random() * words.length)]
-let selectedWordArray = selectedWord.split("")
-
-// Create blanks for each letter in the word to be guessed
-
-let letters = []
-for (letter in selectedWord) {
-    letters.push("_")
-}
-document.getElementById("guessedWord").innerHTML = letters.join("")
-
 // GAME
 
 let gameOn = () => {
+    // document.getElementById("gameResult").style.visibility = "hidden";
+    // document.getElementById("wordWas").style.visibility = "hidden";
+    // document.getElementById("playAgain").style.visibility = "hidden";
+
+
+    // Generate a random word from the list & create letter array of this word
+    let selectedWord = words[Math.floor(Math.random() * words.length)]
+    console.log(selectedWord)
+
+    // Create blanks for each letter in the word to be guessed
+    let letters = []
+    for (letter in selectedWord) {letters.push("_")}
+    document.getElementById("guessedWord").innerHTML = letters.join("")
+    console.log(letters)
+
+    // Keep count of lives left
     document.getElementById("lives").innerHTML = `Lives left: ${lives}`;
+
     // User selection of the letter
     const buttons = document.querySelectorAll(".buttons")
     for (let button of buttons) {
         button.addEventListener("click", () => {
+
             // If letter in word, substitute blank with letter
             if (selectedWord.indexOf(button.innerHTML) !== -1) {
                 let clickedLetter = button.innerHTML.toString();
-                for (let i=0; i<selectedWordArray.length; i++) {
-                    if (selectedWordArray[i] === clickedLetter) {
+                for (let i=0; i<selectedWord.length; i++) {
+                    if (selectedWord[i] === clickedLetter) {
                         letters[i] = clickedLetter
                         document.getElementById("guessedWord").innerHTML = letters.join("");
                         if (letters.includes("_") === false) {
+                            document.getElementById("gameResult").style.visibility = "visible";
                             document.getElementById("gameResult").innerHTML = "You win!";
+                            document.getElementById("playAgain").style.visibility = "visible";
+                            document.getElementById("playAgain").addEventListener("click", () => {
+                                window.location.reload()
+                            })
                         }
                     }
                 }
             }
+
             // If letter not in the word, deduct life
             else {
                 lives -= 1
                 document.getElementById("lives").innerHTML = `Lives left: ${lives}`;
+                console.log(lives)
+                document.getElementById("graphic").src = `../images/hangman-stage${lives}.png`
+                console.log(`../images/hangman-stage${lives}.png`)
                 if (lives === 0) {
+                    document.getElementById("gameResult").style.visibility = `visible`;
                     document.getElementById("gameResult").innerHTML = `game over.`;
+                    document.getElementById("wordWas").style.visibility = `visible`;
                     document.getElementById("wordWas").innerHTML = `The word was: ${selectedWord}`;
                     document.getElementById("playAgain").style.visibility = "visible";
-                    // document.getElementById("playAgain").addEventListener("click", () => {
-                    //
-                    // })
+                    document.getElementById("playAgain").addEventListener("click", () => {
+                        window.location.reload()
+                    })
                 }
             }
         }, {once: true})
@@ -68,9 +84,7 @@ let gameOn = () => {
     }
 }
 
-// TODO: if word guessed, disable buttons and show play again button
-// TODO: if lives lost, disable buttons and show play again button
-// TODO: make Play again button functional
+// TODO: if word guessed or lives lost, disable buttons
 // TODO: add count of lives saved and lives lost
 
 
