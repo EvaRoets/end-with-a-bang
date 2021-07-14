1d4fe06e9aa7f0824061018e119f87eb122dacf4
EwelinaEwa <ewelina.kosciukiewicz@gmail.com>
2021-07-14 02:58:00 +0200

    Generate random words based on selected level

--- js/hangman.js
+++ js/hangman.js
@@ -57,8 +57,11 @@ document.getElementById("reset").addEventListener("click", () => {
     window.location.reload();
 })
 
-// Generate a random word
+// Generate a random word based on the selected level
 
+document.getElementById("easy").addEventListener("click", () => {
+    easyWord()
+}, {once: true})
 
 let easyWord = () => {
     fetch("https://random-word-form.herokuapp.com/random/noun")
@@ -71,6 +74,50 @@ let easyWord = () => {
         })
 }
 
+document.getElementById("difficult").addEventListener("click", () => {
+    difficultWord()
+}, {once: true})
+
+let difficultWord = () => {
+    fetch("https://random-words-api.vercel.app/word")
+        .then((response) => {
+            return response.json();
+        })
+        .then((difficultWord) => {
+            let selectedWord = difficultWord[0].word.toLowerCase();
+            gameOn(selectedWord)
+        })
+}
+
+document.getElementById("countries").addEventListener("click", () => {
+    country()
+}, {once: true})
+
+let country = () => {
+    fetch("https://restcountries.eu/rest/v2/all")
+        .then((response) => {
+            return response.json();
+        })
+        .then((country) => {
+            let selectedWord = country[Math.floor(Math.random()*250)].name.toLowerCase();
+            gameOn(selectedWord)
+        })
+}
+
+document.getElementById("capitals").addEventListener("click", () => {
+    capital()
+}, {once: true})
+
+let capital = () => {
+    fetch("https://restcountries.eu/rest/v2/all")
+        .then((response) => {
+            return response.json();
+        })
+        .then((capital) => {
+            let selectedWord = capital[Math.floor(Math.random()*250)].capital.toLowerCase();
+            gameOn(selectedWord)
+        })
+}
 
 // GAME
 
@@ -79,14 +126,12 @@ let gameOn = (selectedWord) => {
     // Create blanks for each letter in the word to be guessed
     let letters = [];
     console.log(selectedWord);
-    for (let i=0; i<= selectedWord.length; i++) {
-        if (selectedWord[i] === " ") {
-            letters.push(" ")
-        }
-        else if (selectedWord[i] === "-") {
-            letters.push("-")
-        } else {
+    for (let i=0; i< selectedWord.length; i++) {
+        if (alphabet.includes(selectedWord[i])) {
             letters.push("_")
+        }
+        else {
+           letters.push(selectedWord[i])
         }
     }
     document.getElementById("guessedWord").innerHTML = letters.join("");
@@ -104,10 +149,9 @@ let gameOn = (selectedWord) => {
                         letters[i] = clickedLetter
                         document.getElementById("guessedWord").innerHTML = letters.join("");
                         if (letters.includes("_") === false) {
-                            console.log(letters)
                             document.getElementById("gameResult").style.visibility = "visible";
                             document.getElementById("gameResult").innerHTML = "You win!";
-                            document.getElementById("playAgain").style.visibility = "visible";
+                            document.getElementById("playAgainButton").style.visibility = "visible";
                             soulsSaved()
                             document.getElementById("soulsSaved").innerHTML = `Souls saved: ${localStorage.soulsSaved}`
                             document.getElementById("playAgainButton").addEventListener("click", () => {
@@ -120,17 +164,17 @@ let gameOn = (selectedWord) => {
 
             // If letter not in the word, deduct life
             else {
-                if (lives >0) {
+                if (lives > 0) {
                     lives -= 1
                     console.log(lives)
                     document.getElementById("graphic").src = `../images/hangman-stage${lives}.png`;
                 }
                 else if (lives === 0) {
                     document.getElementById("gameResult").style.visibility = `visible`;
-                    document.getElementById("gameResult").innerHTML = `game over.`;
+                    document.getElementById("gameResult").innerHTML = `game over`;
                     document.getElementById("wordWas").style.visibility = `visible`;
                     document.getElementById("wordWas").innerHTML = `The word was: ${selectedWord}`;
-                    document.getElementById("playAgain").style.visibility = "visible";
+                    document.getElementById("playAgainButton").style.visibility = "visible";
                     soulsLost()
                     document.getElementById("soulsLost").innerHTML = `Souls lost: ${localStorage.soulsLost}`
                     document.getElementById("playAgainButton").addEventListener("click", () => {
@@ -139,15 +183,12 @@ let gameOn = (selectedWord) => {
                 }
             }
         }, {once: true})
-        
+
     }
     document.getElementById("soulsSaved").innerHTML = `Souls saved: ${localStorage.soulsSaved}`;
     document.getElementById("soulsLost").innerHTML = `Souls lost: ${localStorage.soulsLost}`;
 }
 
 // TODO: still able to select letters after game over - need a fix
-// TODO: if space or hyphen in word add it to the blanks as uncovered
-// TODO: allow selection (easy, difficult, countries, capitals)
 
 
-easyWord()
