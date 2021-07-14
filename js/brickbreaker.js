518fb5a7a0c9eda7874117d08da6c6c54b988217
EvaRoets <eva_roets@hotmail.com>
2021-07-14 12:38:16 +0200

    Fix lives counter from going below 0

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -1,5 +1,7 @@
 //**VARIABLES**
 const canvas = document.getElementById("gameCanvas"); //create html canvas + enable drawing
+const message = document.getElementById("message");
+const play = document.getElementById("play");
 const context = canvas.getContext("2d");
 const ballRadius = 3; // make ball
 let x = canvas.width / 2; //define starting point in canvas in variables x and y
@@ -21,10 +23,6 @@ const brickOffsetLeft = 5;
 let score = 0;
 let lives = 3;
 
-const message = document.getElementById("message");
-console.log (message)
-const play = document.getElementById("play");
-
 let bricks = []; // loop through all bricks (col and row)
 for (let col = 0; col < brickColCount; col++) {
     bricks[col] = [];
@@ -33,66 +31,7 @@ for (let col = 0; col < brickColCount; col++) {
     }
 }
 
-
-play.addEventListener("click", () => {
-    score = 0
-    message.innerHTML = "Let's go!"
-
-
-
-//**FUNCTIONS**
-const keyDown = (event) => {
-    if (event.key === "Right" || event.key === "ArrowRight") {
-        pressRight = true;
-    } else if (event.key === "Left" || event.key === "ArrowLeft") {
-        pressLeft = true;
-    }
-}
-
-const keyUp = (event) => {
-    if (event.key === "Right" || event.key === "ArrowRight") {
-        pressRight = false;
-    } else if (event.key === "Left" || event.key === "ArrowLeft") {
-        pressLeft = false;
-    }
-}
-
-const mouseMove = (event) => {
-    let mousePositionOnX = event.clientX - canvas.offsetLeft;
-    if (mousePositionOnX > 0 && mousePositionOnX < canvas.width) {
-        paddleX = mousePositionOnX - paddleWidth / 2;
-    }
-}
-
-document.addEventListener("keydown", keyDown, false);//enable keyboard controls paddle
-document.addEventListener("keyup", keyUp, false);
-document.addEventListener("mousemove", mouseMove, false); // enable mouse controls paddle
-
-const collisionDetection = () => { //detect collision
-    for (let col = 0; col < brickColCount; col++) {
-        for (let row = 0; row < brickRowCount; row++) {
-            let brick = bricks[col][row];
-            if (brick.status === 1) {
-                if (x > brick.x &&
-                    x < brick.x + brickWidth &&
-                    y > brick.y &&
-                    y < brick.y + brickHeight) {
-                    yDrawn = -yDrawn; //bounce
-                    brick.status = 0;
-                    score++; //track score
-                    if (score === brickRowCount * brickColCount) {
-                        setTimeout(function () {
-                            message.innerHTML = "You win!🥇";
-                        }, 5000);//wait 10 seconds
-                        document.location.reload();
-                    }
-                }
-            }
-        }
-    }
-}
-
-const drawBall = () => { //create moving ball/ define a drawing loop
+const drawBall = () => {
     context.beginPath();
     context.arc(x, y, ballRadius, 0, Math.PI * 2);
     context.fillStyle = "#ff0000";
@@ -138,69 +77,142 @@ const drawLives = () => {
     context.fillText("❤ = " + lives, canvas.width - 25, canvas.height);
 }
 
-const draw = () => {
-    context.clearRect(0, 0, canvas.width, canvas.height);// clear frame after every interval to make ball instead of line
+window.addEventListener('load', () => {
     drawBall(); //call all draw functions
     drawPaddle();
     drawBricks();
     drawScore();
     drawLives();
-    collisionDetection();
+});
 
-    if (y + yDrawn <= 1) { //when ball goes outside top canvas wall
-        yDrawn = -yDrawn;//reverse direction on y axis = bounce
-    } else if (y + yDrawn > canvas.height - 1) { // when ball goes outside bottom canvas wall
-        if (x > paddleX && //make ball bounce off paddle
-            x < paddleX + paddleWidth) {
-            yDrawn = -yDrawn; //reverse direction on y axis = bounce
-        } else {
-            lives--;
-            if (!lives) {
-                message.innerHTML = "Missed the ball and lost all your lives! ☹"
-                document.location.reload();
-            } else {
-                x = canvas.width / 2;
-                y = canvas.height - 30;
-                xDrawn = 2;
-                yDrawn = -2;
-                paddleX = (canvas.width - paddleWidth) / 2;
-                message.innerHTML = "Missed the ball!☹"
+play.addEventListener("click", () => {
+    score = 0;
+    message.innerHTML = "Let's go!";
+    setTimeout(() => {
+        message.innerHTML = "";
+        }, 1500)
+
+//**FUNCTIONS**
+    const keyDown = (event) => {
+        if (event.key === "Right" || event.key === "ArrowRight") {
+            pressRight = true;
+        } else if (event.key === "Left" || event.key === "ArrowLeft") {
+            pressLeft = true;
+        }
+    }
+
+    const keyUp = (event) => {
+        if (event.key === "Right" || event.key === "ArrowRight") {
+            pressRight = false;
+        } else if (event.key === "Left" || event.key === "ArrowLeft") {
+            pressLeft = false;
+        }
+    }
+
+    const mouseMove = (event) => {
+        let mousePositionOnX = event.clientX - canvas.offsetLeft;
+        if (mousePositionOnX > 0 && mousePositionOnX < canvas.width) {
+            paddleX = mousePositionOnX - paddleWidth / 2;
+        }
+    }
+
+    document.addEventListener("keydown", keyDown, false);//enable keyboard controls paddle
+    document.addEventListener("keyup", keyUp, false);
+    document.addEventListener("mousemove", mouseMove, false); // enable mouse controls paddle
+
+    const collisionDetection = () => { //detect collision
+        for (let col = 0; col < brickColCount; col++) {
+            for (let row = 0; row < brickRowCount; row++) {
+                let brick = bricks[col][row];
+                if (brick.status === 1) {
+                    if (x > brick.x &&
+                        x < brick.x + brickWidth &&
+                        y > brick.y &&
+                        y < brick.y + brickHeight) {
+                        yDrawn = -yDrawn; //bounce
+                        brick.status = 0;
+                        score++; //track score
+                        if (score === brickRowCount * brickColCount) {
+                            setTimeout(function () {
+                                message.innerHTML = "You win!🥇";
+                            }, 5000);//wait 5 seconds
+                            document.location.reload();
+                        }
+                    }
+                }
             }
         }
     }
 
-    if (x + xDrawn <= 1 //when ball goes outside left canvas wall
-        || x + xDrawn > canvas.width - 1) {  ///when ball goes outside right canvas wall
-        xDrawn = -xDrawn; //reverse direction on x axis = bounce
-    }
 
-    x += xDrawn; //update x and y to make ball appear in new position on every update
-    y += yDrawn;
+    const draw = () => {
+        context.clearRect(0, 0, canvas.width, canvas.height);// clear frame after every interval to make ball instead of line
+        collisionDetection();
+        drawBall(); //call all draw functions
+        drawPaddle();
+        drawBricks();
+        drawScore();
+        drawLives();
 
-    if (pressRight) {//enable keyboard controls paddle
-        paddleX += 18; // make paddle move right 18px
-        if (paddleX + paddleWidth > canvas.width) {
-            paddleX = (canvas.width - 1) - paddleWidth;
+        if (y + yDrawn <= 1) { //when ball goes outside top canvas wall
+            yDrawn = -yDrawn;//reverse direction on y axis = bounce
+        } else if (y + yDrawn > canvas.height - 1) { // when ball goes outside bottom canvas wall
+            if (x > paddleX && //make ball bounce off paddle
+                x < paddleX + paddleWidth) {
+                yDrawn = -yDrawn; //reverse direction on y axis = bounce
+            } else {
+                lives--;
+                if (lives <= 0) {
+                    lives = 0;
+                    // wait with moving ball loading
+                    message.innerHTML = "You missed the ball and lost all your lives! ☹";
+                    setTimeout(function () {
+                        document.location.reload()
+                    }, 70000)
+                } else {
+                    x = canvas.width / 2;
+                    y = canvas.height - 30;
+                    xDrawn = 2;
+                    yDrawn = -2;
+                    paddleX = (canvas.width - paddleWidth) / 2;
+                    message.innerHTML = "Oh, missed the ball! ☹"
+                }
+            }
         }
-    } else if (pressLeft) {
-        paddleX -= 18; // make paddle move left 18px
-        if (paddleX < 0) {
-            paddleX = 1;
+
+        if (x + xDrawn <= 1 //when ball goes outside left canvas wall
+            || x + xDrawn > canvas.width - 1) {  ///when ball goes outside right canvas wall
+            xDrawn = -xDrawn; //reverse direction on x axis = bounce
         }
+
+        x += xDrawn; //update x and y to make ball appear in new position on every update
+        y += yDrawn;
+
+        if (pressRight) {//enable keyboard controls paddle
+            paddleX += 10; // make paddle move right 18px
+            if (paddleX + paddleWidth > canvas.width) {
+                paddleX = (canvas.width - 1) - paddleWidth;
+            }
+        } else if (pressLeft) {
+            paddleX -= 10; // make paddle move left 18px
+            if (paddleX < 0) {
+                paddleX = 1;
+            }
+        }
+        requestAnimationFrame(draw);
     }
-    requestAnimationFrame(draw);
-}
-draw();
+    draw();
 });
 
 
 //Phase 5 - priorities
-//TODO functionality play button
-//TODO update message board
-//TODO styling buttons and message board
-
+// add timer to game over display
+// add timer to beginning game
+// add timer to ball moving
+//
 
 //Phase 6 - extras
+//TODO add random messages when each brick breaks
 //TODO convert for loops into for of/for in
 //TODO make mouse movement more sensitive
 //TODO make canvas less pixelated
